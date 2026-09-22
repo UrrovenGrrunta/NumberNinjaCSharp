82ed2d14dae862dfc14956a6f42bcf3ff5f8281f
Timur <timbro2312@gmail.com>
2026-09-22 19:33:17 +0100

    Backup Commit

--- NumberNinjaCSharp/Game/GameController.cs
+++ NumberNinjaCSharp/Game/GameController.cs
@@ -3,6 +3,7 @@ using System;
 public class GameController
 {
     private Difficulty _currentDifficulty;
+    private GameState _currentState = GameState.Menu;
 
     public Difficulty GetDifficulty()
     {
@@ -13,4 +14,18 @@ public class GameController
     {
         _currentDifficulty = difficulty;
     }
-}
+    public GameState GetGameState()
+    {
+        return _currentState;
+    }
+    public GameState StartGame()
+    {
+        _currentState = GameState.Playing;
+        return _currentState;
+    }
+    public GameState EndGame()
+    {
+        _currentState = GameState.GameOver;
+        return _currentState;
+    }
+}   
